7bfbcc78f2a58baa6660fe958dccd5fcd4cb1bb0
Jaikunwar Sidhu <jaikunwarsidhu@gmail.com>
2020-04-13 17:29:17 -0400

    Added Flock And Flocking Agent

---
+++ FlockingAssignment/Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class FlockAgent : MonoBehaviour
+{
+
+    Collider2D agentscollider;
+    public Collider2D AgentCollider { get { return agentscollider; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentscollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+     public void Move(Vector2 velocity)
+     {
+
+        transform.up = velocity;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+     }
+}
